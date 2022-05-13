d1704d426b95776802142b55eb33159598b20d3f
unknown <cst119@CST.SIAST.SK.CA>
2022-05-13 09:50:12 -0600

    Created a skeleloton of the Team class to start making Unit Tests

---
+++ src/main/java/com/cosacpmg/Team.java
@@ -0,0 +1,18 @@
+package com.cosacpmg;
+
+public class Team
+{
+	private String teamName, city, area, coachName, coachNumber;
+//	ArrayList<Player> playerList;
+	
+	
+	public Team(String teamName, String city, String area, String coachName, String coachNumber)
+	{
+		this.teamName = teamName;
+		this.city = city;
+		this.area = area;
+		this.coachName = coachName;
+		this.coachNumber = coachNumber;
+	}
+	
+}
